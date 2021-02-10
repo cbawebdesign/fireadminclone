d56b50f7db0d855e1414a99bd134d5c79b09a1c9
chris <cbawebdesign1@gmail.com>
2021-02-10 15:03:58 -0800

    commit

---
+++ projects/fire-admin/src/lib/services/storage.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+
+@Injectable()
+export class StorageService {
+  ref(path: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor(private storage: AngularFireStorage) { }
+
+  get(path: string) {
+    return this.storage.ref(path);
+  }
+
+  upload(path: string, file: File): AngularFireUploadTask {
+    return this.get(path).put(file);
+  }
+
+  delete(path: string) {
+    return this.get(path).delete();
+  }
+
+}
